7a2048bc9d67f021adce36470ab9d911079c8ec5
Sparsh Gupta <47032539+sgupta-1@users.noreply.github.com>
2022-08-06 00:58:53 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,14 @@ export default function About() {
   return (
     <div>
       <Header/>
+      <div>
+        <div className="bg-about h-60 text-white">
+        <div className='bg-black opacity-60 mx-6'>
+          <div className='font-bold flex justify-center text-5xl'>About Us</div><br/>
+          <div className='uppercase flex justify-center text-3xl'>We offer quality that is remembered long after the price is forgotten.</div>
+        </div>
+        </div>
+      </div>
       <Footer/>
     </div>
   )
